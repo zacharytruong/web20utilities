e318db62a399f43cc099632b1972fba1bebff87a
Zachary Truong <zacharytruong@gmail.com>
2022-02-17 17:22:45 -0600

    Initial build

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# web20utilities
+# Side Web 2.0 XML Generator
+
+In-progress project.
+
+The web app will help generate the import XML file for **Side web 2.0**. The XML currently allows users to import the following custom post types (CPT):
+1. Members (Team Member)
+2. Member-profiles (CAPP)
+
+Future plan:
+* More in-depth ‘members’ input.
+* More in-depth ‘member-profiles’ input.
+* Community details generator.
+* Retrieve ‘members’ data directly from SFDC.
+
+## Instruction (of the newest version)
+
+## Author, Contributors
+
+* Zachary Truong
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
