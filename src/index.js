72619bbbd5cf18ffa6d012692c83f1cc580b808c
Zachary Truong <zacharytruong@gmail.com>
2022-02-19 05:15:10 -0600

    drag n drop working

--- src/index.js
+++ src/index.js
@@ -17,16 +17,77 @@ import {memberProfilesXML} from './memberProfiles/memberProfilesXML.js';
 (function basicLayout() {
   
   const mainContent = document.getElementById('mainContent');
-  mainContent.style.background = 'rgba( 255, 255, 255, 0.25 )';
-  mainContent.style.boxShadown = '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )';
-  mainContent.style.backdropFilter = 'blur( 4px )';
-  mainContent.style['-webkit-backdrop-filter'] = 'blur( 4px )';
-  mainContent.style.border = '.1rem solid rgba( 255, 255, 255, 0.18 )';
+  
+  // mainContent.style.background = 'rgba( 255, 255, 255, 0.25 )';
+  // mainContent.style.boxShadown = '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )';
+  // mainContent.style.backdropFilter = 'blur( 4px )';
+  // mainContent.style['-webkit-backdrop-filter'] = 'blur( 4px )';
+  // mainContent.style.border = '.1rem solid rgba( 255, 255, 255, 0.18 )';
   mainContent.style.display = 'flex';
   mainContent.style['align-items'] = 'center';
   mainContent.style['justify-content'] = 'center';
   mainContent.style.minWidth = '100vw';
   mainContent.style.minHeight = '100vh';
+
+})();
+
+(function uploadForm() {
+
+  // const mainContent = document.getElementById('mainContent');
+  const uploadForm = document.createElement('form');
+  mainContent.appendChild(uploadForm);
+  const uploadBtn = document.createElement('input');
+  uploadBtn.id = 'upload';
+  uploadBtn.type = 'file';
+  uploadBtn.addEventListener('change', handleUploadFile);
+  uploadForm.appendChild(uploadBtn);
+  uploadBtn.style.padding = `1.5rem 3rem`;
+  uploadBtn.style.cursor = 'pointer';
+  uploadBtn.style.fontSize = '2rem';
+  uploadBtn.style.background = '#3f4241';
+  uploadBtn.style.color = '#FFFFFF';
+  uploadBtn.style['border-radius'] = '.7rem';
+  mainContent.appendChild(uploadForm);
+  const dropArea = document.createElement('div');
+  dropArea.id = 'drop-area';
+  dropArea.style.width = '30rem';
+  dropArea.style.height = '30rem';
+  dropArea.style.background = 'rgba(63, 66, 65, .5)';
+  dropArea.style.borderRadius = '2rem';
+  dropArea.style.margin = '1rem';
+  const dropNotice = document.createElement('p');
+  dropNotice.textContent = 'Drop Your File Here';
+  dropNotice.style.fontSize = '2rem';
+  dropNotice.style.textAlign = 'center';
+  dropNotice.style.padding = '2rem';
+  dropNotice.style.userSelect = 'none';
+  dropArea.appendChild(dropNotice);
+  uploadForm.appendChild(dropArea);
+
+  function handleUploadFile(e) {
+    console.log(e.target.files)
+  }
+  
+  dropArea.addEventListener('dragover', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    // Style the drag-and-drop as a "copy file" operation.
+    event.dataTransfer.dropEffect = 'copy';
+  });
+  
+  dropArea.addEventListener('drop', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const fileList = event.dataTransfer.files;
+    dropNotice.textContent = fileList[0].name;
+    console.log(fileList);
+  });
+})();
+
+(function dropFile() {
+
+    
+
 
 })();
 
