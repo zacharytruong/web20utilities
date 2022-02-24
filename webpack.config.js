b396cadd112813b4d6878600e7c6a7211be8107b
Zachary Truong <zacharytruong@gmail.com>
2022-02-23 19:57:53 -0600

    remove extras

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     index: './src/index.js',
   },
-  devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
   module: {
     rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
       {
         test: /\.(csv|tsv)$/i,
         use: ['csv-loader'],
@@ -44,7 +18,7 @@ module.exports = {
     ],
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: "main.js",
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
